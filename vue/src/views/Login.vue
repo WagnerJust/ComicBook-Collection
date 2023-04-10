9838a1ef46ac494f6aebb65409ac2b4b31226a52
Maxwell MacFarlane <maxwell.macfarlane@gmail.com>
2023-04-10 14:45:00 -0600

    updated the login and registration pages

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -1,25 +1,34 @@
 <template>
-  <div id="login">
-    <form @submit.prevent="login">
-      <h1 >Please Sign In</h1>
-      <div role="alert" v-if="invalidCredentials">
-        Invalid username and password!
+  <div id="container">
+    <div id="login-banner">
+      <img src="..\public\manageMyComics.png">
+    </div>
+    <div id="login">
+      <form @submit.prevent="login">
+        <h1 >LOGIN</h1>
+        <div role="alert" v-if="invalidCredentials">
+          Invalid username and password!
+        </div>
+        <div role="alert" v-if="this.$route.query.registration">
+          Thank you for registering, please sign in.
+        </div>
+        <div class="form-input-group">
+          <input type="text" id="username" placeholder="username" v-model="user.username" required autofocus />
+        </div>
+        <div class="form-input-group">
+          <input type="password" id="password" placeholder="password" v-model="user.password" required />
+        </div>
+        <button type="submit">SIGN IN</button>
+        
+      </form>
+    </div>
+    <div id="create-user">
+      <div>
+        <div id="welcome">Welcome Back!</div>
+        <p>Not Registered?</p>
+        <router-link :to="{ name: 'register' }" tag="button">SIGN UP</router-link>
       </div>
-      <div role="alert" v-if="this.$route.query.registration">
-        Thank you for registering, please sign in.
-      </div>
-      <div class="form-input-group">
-        <label for="username">Username</label>
-        <input type="text" id="username" v-model="user.username" required autofocus />
-      </div>
-      <div class="form-input-group">
-        <label for="password">Password</label>
-        <input type="password" id="password" v-model="user.password" required />
-      </div>
-      <button type="submit">Sign in</button>
-      <p>
-      <router-link :to="{ name: 'register' }">Need an account? Sign up.</router-link></p>
-    </form>
+    </div>
   </div>
 </template>
 
@@ -62,6 +71,73 @@ export default {
 </script>
 
 <style scoped>
+
+#container {
+  background-color: #F5DE87;
+  display: grid;
+  grid-template-areas: 
+  "banner banner"
+  "login register"
+  "login register";
+  gap: 1rem;
+  border: 5px solid black;
+  height: 95vh;
+}
+
+#login-banner {
+  grid-area: banner;
+  display: flex;
+  height: 30vh;
+  width: 95%;
+  background-image: url("https://cdn.pixabay.com/photo/2016/03/06/04/14/comics-1239698_960_720.jpg");
+  border-style: solid;
+  justify-self: center;
+  margin-top: 1rem;
+  justify-content: center;
+  align-items: center;
+}
+
+#login-banner > img {
+  display: flex;
+  height: 80%;
+}
+
+#login {
+  display: flex;
+  background-color: white;
+  height: 25rem;
+  width: 25rem;
+  grid-area: login;
+  border: 5px solid black;
+  align-self: center;
+  justify-self: center;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+#login > form {
+
+}
+
+#welcome {
+  font-size: 3rem;
+}
+
+#create-user {
+  display: flex;
+  background-color: #BC545E;
+  height: 25rem;
+  width: 25rem;
+  align-self: center;
+  justify-self: center;
+  grid-area: register;
+  border: 5px solid black;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
 .form-input-group {
   margin-bottom: 1rem;
 }
