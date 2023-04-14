48b17779234e281e82df462e57d1220a6ae61b0f
Maxwell MacFarlane <maxwell.macfarlane@gmail.com>
2023-04-14 13:29:03 -0600

    add a collection form is complete

--- vue/src/components/AddCollection.vue
+++ vue/src/components/AddCollection.vue
@@ -1,19 +1,23 @@
 <template>
-    <form class="collection-form" v-on:submit.prevent>
-        <p class="field">
+    <div class="container">
+        <button id="show-form-button" v-if="showForm === false" v-on:click.prevent="showForm = true">
             - Add New Collection -
-        </p>
-        <div class="field">
-            <input type="text" v-model="collection.collectionName" />
-        </div>
-        <div class="field">
-            <label for="content">Public? </label>
-            <input type="checkbox" v-model="collection.public" />
-        </div>
-        <div class="field">
-            <button type="submit" v-on:click="saveCollection()">Save Collection</button>
-        </div>
-    </form>
+        </button>
+        <form class="collection-form" name="collection-form" autocomplete="off" v-if="showForm === true">
+            <div class="field">
+                <input required id="collection-form-name-input" name="collection-form-name-input" type="text" placeholder="Collection Name" v-model="collection.collectionName" onInvalid="this.setCustomValidity('Your collection must have a name.')" oninput="setCustomValidity('')" />
+            </div>
+            <div id="public-section" class="field">
+                <label id="public-text" for="checkbox">Public? </label>
+                <input id="switch" type="checkbox" v-model="collection.public" /><label id="testLabel" for="switch">Toggle</label>
+            </div>
+            <div class="field">
+                <button id="save-collection-button" type="submit" v-on:click="saveCollection()">Save Collection</button>
+                <button id="reset-collection-button" type="reset" >Reset Form</button>              
+            </div>
+            <a id="cancel-button" v-on:click="showForm = false">Cancel</a> 
+        </form>
+    </div>
 </template>
 
 <script>
@@ -23,6 +27,7 @@ export default {
     name: "add-collection",
     data() {
         return {
+            showForm: false,
             collection: {
                 userId: this.$store.state.user.id,
                 collectionId: '',
@@ -32,11 +37,16 @@ export default {
     },
     methods: {
         saveCollection() {
-            collectionService.createCollection(this.collection).then(response => {
-                if (response.status === 201) {
-                    this.$router.go("/collections")
-                }
-            })
+            let x = document.forms["collection-form"]["collection-form-name-input"].value;
+            if (x == "") {
+                return false;
+            } else {
+                collectionService.createCollection(this.collection).then(response => {
+                    if (response.status === 201) {
+                        this.$router.go("/collections")
+                    }
+                })                
+            }
         }
     }
 }
@@ -44,32 +54,127 @@ export default {
 
 <style scoped>
 
-    .collection-form {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        margin-top: 1rem;
-        text-align: left;
-        font-family: 'Montserrat', Helvetica, sans-serif;
-    }
+form-text validate[required] form-validation-error {
+    position: left;
+}
 
-    .field {
-        margin: .2rem;
-    }
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 1rem;
+    text-align: left;
+    font-family: 'Montserrat', Helvetica, sans-serif;
+}
 
-    button {
-        cursor: pointer;
-        border: 0;
-        border-radius: 4px;
-        font-weight: 600;
-        margin: 1 10px;
-        width: 8rem;
-        padding: 10px 0;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
-        transition: 0.4s;
-        color: black;
-        background-color: rgba(255, 255, 255, 1);
-        border: 1px solid black;
-        font-family: 'Montserrat', Helvetica, sans-serif;
-    }
+.field {
+    display: flex;
+    justify-content: center;
+    margin: 1rem;
+}
+
+#collection-form-name-input {
+    height: 3rem;
+    width: 25rem;
+    font-size: 1.5rem;
+    text-align: center;
+}
+
+#collection-form-name-input:focus::placeholder {
+    color: transparent;
+}
+
+input::placeholder {
+    text-align: center;
+}
+
+#public-section {
+    display: flex;
+    align-items: center;
+}
+
+#public-text {
+    font-size: 2rem;
+    font-weight: bold;
+    margin-right: 1rem;
+}
+
+button {
+    cursor: pointer;
+    border: 0;
+    border-radius: 4px;
+    font-weight: 600;
+    margin: 1 10px;
+    width: 8rem;
+    padding: 10px 0;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+    transition: 0.4s;
+    color: black;
+    background-color: rgba(255, 255, 255, 1);
+    border: 1px solid black;
+    font-family: 'Montserrat', Helvetica, sans-serif;
+}
+
+#reset-collection-button {
+    background-color: rgba(236, 29, 36, 0.6);
+}
+
+#reset-collection-button:hover {
+    background-color: rgba(236, 29, 36, 1);
+}
+
+button:hover {
+    color: white;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+    background-color: rgba(236, 29, 36, 1);
+}
+
+#save-collection-button {
+    margin-right: 1rem;
+}
+
+#cancel-button {
+    display: flex;
+    justify-content: center;
+    cursor: pointer;
+    margin-top: 1rem;
+}
+
+/* testing switch */
+input[type=checkbox]{
+	height: 0;
+	width: 0;
+	visibility: hidden;
+}
+
+#testLabel {
+	cursor: pointer;
+	text-indent: -9999px;
+	width: 4rem;
+	height: 2rem;
+	background: grey;
+	display: block;
+	position: relative;
+}
+
+label:after {
+	content: '';
+	position: absolute;
+	top: 0px;
+	left: 0px;
+	width: 2rem;
+	height: 2rem;
+	background: #fff;
+	transition: 0.2s;
+}
+
+input:checked + #testLabel {
+	background: #10e710;
+}
+
+input:checked + #testLabel:after {
+	left: calc(100%);
+	transform: translateX(-100%);
+}
+
 </style>
