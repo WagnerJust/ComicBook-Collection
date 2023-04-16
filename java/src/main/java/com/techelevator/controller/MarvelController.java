2fc4704b580735cd991ba14819857363256389e8
David Sensendorf <david1391@gmail.com>
2023-04-16 12:24:28 -0700

    Added controller and endpoints for marvel API usage

---
+++ java/src/main/java/com/techelevator/controller/MarvelController.java
@@ -0,0 +1,34 @@
+package com.techelevator.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.techelevator.model.Comic;
+import com.techelevator.services.RestMarvelService;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@PreAuthorize("isAuthenticated")
+@CrossOrigin
+public class MarvelController {
+    RestMarvelService restMarvelService;
+
+    public MarvelController(RestMarvelService marvelService){
+        this.restMarvelService = marvelService;
+    }
+
+
+    @PreAuthorize("hasAnyRole('USER','PREMIUM')")
+    @GetMapping("/marvel/{series}/{issueNo}")
+    public List<Comic> searchMarvelBySeriesAndIssueNo(@PathVariable String series, @PathVariable String issueNo) throws JsonProcessingException {
+        return restMarvelService.searchComicsBySeriesAndIssueNo(series, issueNo);
+    }
+
+    @PreAuthorize("hasAnyRole('USER','PREMIUM')")
+    @GetMapping("/marvel/upc/{upc}")
+    public Comic getMarvelComicByUpc(@PathVariable String upc) throws JsonProcessingException {
+        return restMarvelService.getComicByUpc(upc);
+    }
+}
