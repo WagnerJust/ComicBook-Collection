d13cb16391cc6bc8cf207a26fd22a105c0ff0bbe
Justin Wagner <justhwagner@gmail.com>
2023-04-17 16:35:00 -0600

    Stats view bugs fixed

--- java/src/main/java/com/techelevator/dao/JdbcStatsDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcStatsDao.java
@@ -20,20 +20,61 @@ public class JdbcStatsDao implements StatsDao{
 
 
     @Override
-    public List<Statistics> collectionStatistics(int collectionId) {
+    public List<Statistics> collectionCharacterStats(int collectionId) {
         List<Statistics> statisticsList = new ArrayList<>();
-        String sql = "select count(character_comic.character_id) as num_comics_character , character_table.name, count(comic_data.author) as num_comics_author, comic_data.author, count(comic_data.series) as num_comics_series, comic_data.series\n" +
+        String sql = "select count(comic_data.author) as num_comics_author, comic_data.author\n" +
+                "from comic_data\n" +
+                "join comic_collection ON comic_collection.comic_data_id = comic_data.comic_data_id\n" +
+                "join collection ON collection.collection_id = comic_collection.collection_id\n" +
+                "where collection.collection_id = ?\n" +
+                "group by comic_data.author\n" +
+                "order by 1 desc;";
+
+        SqlRowSet results =  jdbcTemplate.queryForRowSet(sql, collectionId);
+
+        while(results.next()){
+            statisticsList.add(mapRowToStatistics(results, "character"));
+        }
+        return statisticsList;
+    }
+
+    @Override
+    public List<Statistics> collectionAuthorStats(int collectionId) {
+        List<Statistics> statisticsList = new ArrayList<>();
+
+
+        String sql = "select count(comic_data.author) as num_comics_author, comic_data.author\n" +
+                "from comic_data\n" +
+                "join comic_collection ON comic_collection.comic_data_id = comic_data.comic_data_id\n" +
+                "join collection ON collection.collection_id = comic_collection.collection_id\n" +
+                "where collection.collection_id = ?\n" +
+                "group by comic_data.author\n" +
+                "order by 1 desc;";
+
+        SqlRowSet results =  jdbcTemplate.queryForRowSet(sql, collectionId);
+        while(results.next()){
+            statisticsList.add(mapRowToStatistics(results, "author"));
+        }
+        return statisticsList;
+    }
+
+    @Override
+    public List<Statistics> collectionSeriesStats(int collectionId) {
+        List<Statistics> statisticsList = new ArrayList<>();
+        String sql = "select count(comic_data.series) as num_comics_series, comic_data.series\n" +
                 "from character_table\n" +
                 "join character_comic ON character_comic.character_id = character_table.character_id\n" +
                 "join comic_data ON comic_data.comic_data_id = character_comic.comic_data_id\n" +
                 "join comic_collection ON comic_collection.comic_data_id = comic_data.comic_data_id\n" +
                 "join collection ON collection.collection_id = comic_collection.collection_id\n" +
                 "where collection.collection_id = ?\n" +
-                "group by character_table.name, comic_data.author,  comic_data.series;";
+                "group by comic_data.series\n" +
+                "order by 1 desc;";
 
-        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, collectionId);
+        SqlRowSet results =  jdbcTemplate.queryForRowSet(sql, collectionId);
+
         while(results.next()){
-            statisticsList.add(mapRowToStatistics(results));
+            statisticsList.add(mapRowToStatistics(results, "series"));
         }
         return statisticsList;
     }
@@ -51,20 +92,37 @@ public class JdbcStatsDao implements StatsDao{
 
         SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
         while(results.next()){
-            statisticsList.add(mapRowToStatistics(results));
+            statisticsList.add(mapRowToStatistics(results, "all"));
         }
         return statisticsList;
 
     }
 
-    private Statistics mapRowToStatistics(SqlRowSet results) {
+    private Statistics mapRowToStatistics(SqlRowSet results, String type) {
         Statistics statistics = new Statistics();
-        statistics.setNum_comics_character(results.getInt("num_comics_character"));
-        statistics.setName(results.getString("name"));
-        statistics.setNum_comics_author(results.getInt("num_comics_author"));
-        statistics.setAuthor(results.getString("author"));
-        statistics.setNum_comics_series(results.getInt("num_comics_series"));
-        statistics.setSeries(results.getString("series"));
+        switch(type){
+            case "character":
+                statistics.setNum_comics_character(results.getInt(1));
+                statistics.setName(results.getString(2));
+                break;
+            case "author":
+                statistics.setNum_comics_author(results.getInt(1));
+                statistics.setAuthor(results.getString(2));
+                break;
+            case "series":
+                statistics.setNum_comics_series(results.getInt(1));
+                statistics.setSeries(results.getString(2));
+//            case "all":
+//                statistics.setNum_comics_character(results.getInt("num_comics_character"));
+//                statistics.setName(results.getString("name"));
+//                statistics.setNum_comics_author(results.getInt("num_comics_author"));
+//                statistics.setAuthor(results.getString("author"));
+//                statistics.setNum_comics_series(results.getInt("num_comics_series"));
+//                statistics.setSeries(results.getString("series"));
+            default:
+                break;
+        }
+
         return statistics;
     }
 }
