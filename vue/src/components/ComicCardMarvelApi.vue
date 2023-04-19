e470d99997449e26f87dff6a5660131754eca34a
Maxwell MacFarlane <maxwell.macfarlane@gmail.com>
2023-04-19 15:01:31 -0600

    fixed issue# on the front end

--- vue/src/components/ComicCardMarvelApi.vue
+++ vue/src/components/ComicCardMarvelApi.vue
@@ -3,7 +3,7 @@
         <add-comic-marvel-api class="add-button" :comic="comic"  v-if="$store.state.token != ''" />
         <div class="image-wrap">
             <router-link v-bind:to="{ name: 'comicdetails', params: { id: comic.upc } }"><img class="comic-image" v-bind:src="'http://i.annihil.us/u/prod/marvel/i/mg/' + comic.imageURL + '/detail.jpg'" /></router-link>
-            <h3 class="comic-series">{{ comic.seriesName }}</h3>
+            <h3 class="comic-series">{{ comic.seriesName }} #{{comic.issueNumber}}</h3>
             <p class="comic-creator" v-if="comic.author != null" >Writer: {{ comic.author }}</p>
             <p class="comic-creator" v-if="comic.artist != null" >Penciler: {{ comic.artist }}</p>
             <p class="comic-publish-date" v-if="comic.publish_date != null" >Published: {{ comic.publish_date }}</p>
